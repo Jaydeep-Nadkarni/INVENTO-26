98d1d591f70f9127a6ac0901caee64c8fea0d44b
Jaydeep Nadkarni <jaydeepnadkarni123@gmail.com>
2026-01-09 13:26:34 +0530

    feat: Reintroduce DataProvider and AdminAuthProvider in main.jsx for improved context management; streamline App component structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AdminAuthProvider } from './admin/context/AuthContext'
+import { DataProvider } from './admin/context/DataContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <DataProvider>
+      <AdminAuthProvider>
+        <App />
+      </AdminAuthProvider>
+    </DataProvider>
   </StrictMode>,
 )
